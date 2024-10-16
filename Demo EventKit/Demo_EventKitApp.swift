3a8ec89516b53b6552e898ec0b694575c86565a1
Ngoc Khanh Phan <31550323+ngockhanhphan@users.noreply.github.com>
2024-10-16 14:43:50 +0700

    Init project

---
+++ Demo EventKit/Demo_EventKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_EventKitApp.swift
+//  Demo EventKit
+//
+//  Created by Khanh Phan on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_EventKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo EventKitTests/Demo_EventKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_EventKitTests.swift
+//  Demo EventKitTests
+//
+//  Created by Khanh Phan on 16/10/24.
+//
+
+import Testing
+@testable import Demo_EventKit
+
+struct Demo_EventKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
